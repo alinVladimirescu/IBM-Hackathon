65f9461434d5546381a240849881da0d4be61139
Javier Borrajo <jborrajof@gmail.com>
2025-11-21 18:39:31 +0000

    Backend created.

--- project.py
+++ project.py
@@ -1,2 +1,24 @@
-for i in range(1000000):
-    print("Relax")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Employee(BaseModel):
+    id: int
+    name: str
+    email: str
+
+
+@app.get("/test")
+async def get_something():
+    return {"response": "got soemting from db"}
+
+@app.post("/test")
+async def post_something(employee: Employee):
+    print(employee)
+    return {"status": "created succesfully", "employee": employee}
+
+@app.put("/test{id}")
+async def update_something(new_employee: Employee):
+    print(new_employee)
+    return {"status": "updated", "employee": new_employee}
